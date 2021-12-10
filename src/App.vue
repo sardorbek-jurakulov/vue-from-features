67b297c58c2938a02ed259954990583df4ea3e05
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2021-12-10 12:00:20 +0500

    WIP

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,26 @@
                     <!-- Mail -->
                     <!-- Password -->
                     <!-- Store Data? Yes/No -->
-
+                    <form action="https://echo.htmlacademy.ru" method="POST" @submit.prevent="submitted">
+                      <label for="firstName">First Name</label><br>
+                      <input type="text" name="firstName" id="firstName" placeholder="Jon" v-model="firstName">
+                      <br><br>
+                      <label for="lastName">Last Name</label><br>
+                      <input type="text" name="lastName" id="lastName" placeholder="Doe" v-model="lastName">
+                      <br><br>
+                      <label for="email">Email</label><br>
+                      <input type="email" name="email" id="email" placeholder="example@gmail.com" v-model="email">
+                      <br><br>
+                      <label for="email">Password</label><br>
+                      <input type="password" name="password" id="password" v-model="password">
+                      <br><br>
+                      <p>Store Data?</p>
+                      <label for="storeDataYes">Yes</label><input type="radio" name="storeData" value="yes" id="storeDataYes" v-model="isStoreData">
+                      &nbsp;&nbsp;&nbsp;
+                      <label for="storeDataNo">No</label><input type="radio" name="storeData" value="no" id="storeDataNo" v-model="isStoreData">
+                      <br><br><br>
+                      <input type="submit" value="submit" name="submit">
+                    </form>
                     <!-- Exercise 2 -->
                     <!-- Only display the Form if it has NOT been submitted -->
                     <!-- Display the Data Summary ONCE the Form HAS been submitted -->
@@ -21,17 +40,17 @@
             </div>
         </form>
         <hr>
-        <div class="row">
+        <div class="row" v-if="isSubmitted">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <div class="panel panel-default">
                     <div class="panel-heading">
                         <h4>Your Data</h4>
                     </div>
                     <div class="panel-body">
-                        <p>Full Name: </p>
-                        <p>Mail: </p>
-                        <p>Password: </p>
-                        <p>Store in Database?: </p>
+                        <p>Full Name: {{firstName}} {{ lastName }}</p>
+                        <p>Mail: {{ email }}</p>
+                        <p>Password: {{ password }}</p>
+                        <p>Store in Database?: {{ isStoreData }}</p>
                     </div>
                 </div>
             </div>
@@ -41,6 +60,21 @@
 
 <script>
     export default {
+      data() {
+        return {
+          firstName: '',
+          lastName: '',
+          email: '',
+          password: '',
+          isStoreData: 'yes',
+          isSubmitted: false,
+        }
+      },
+      methods: {
+        submitted() {
+          this.isSubmitted = true;
+        }
+      }
     }
 </script>
 
